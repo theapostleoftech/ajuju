84f27808fbd23eed7bc6266f9bb2232c99109fab
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-10-13 14:10:13 +0100

    user model admin

--- ajuju/settings.py
+++ ajuju/settings.py
@@ -26,6 +26,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.UserModel'
+
 # Application definition
 
 INSTALLED_APPS = [
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,87 @@
-from django.contrib import admin
-
 # Register your models here.
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import UserModel
+
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all required fields, plus a repeated password.
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = UserModel
+        fields = ('username', 'email', 'full_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """
+    A form for updating users. Includes all fields on the user, but replaces
+    the password field with admin's password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = UserModel
+        fields = ('username', 'email', 'full_name', 'password', 'is_active', 'is_staff', 'is_superuser')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+@admin.register(UserModel)
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom admin class for the UserModel. Enhances the default UserAdmin
+    with custom forms and fieldsets.
+    """
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # Fields to be used in displaying the User model.
+    list_display = ('username', 'email', 'full_name', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+
+    # Define fieldsets for editing users
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    # Define fieldsets for adding users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'full_name', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'email', 'full_name')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
