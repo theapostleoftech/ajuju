4fca1c7792a15aeaac0a11c493e25e334887c7ce
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-10-13 14:05:16 +0100

    user account manager setup

--- users/managers.py
+++ users/managers.py
@@ -2,20 +2,35 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError('The Username must be set')
         if not email:
-            raise ValueError("Users must have an email address.")
-
-        user = self.model(email=self.normalize_email(email))
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
-
+        user.save()
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email,password=password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
+    def create_superuser(self, username, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
-        return user
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(username, email, password, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -2,10 +2,21 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+from core.models import default_uuid
 from users.managers import UserManager
 
 
 class UserModel(AbstractBaseUser, PermissionsMixin):
+    """
+    This is the user model for ajuju
+    """
+    id = models.CharField(
+        default=default_uuid,
+        editable=False,
+        unique=True,
+        max_length=36,
+        primary_key=True,
+    )
     username = models.CharField(
         max_length=255,
         unique=True,
@@ -29,7 +40,9 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "username"
+
+    REQUIRED_FIELDS = ["email"]
 
     def get_full_name(self):
         return self.full_name
