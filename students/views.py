ce2eb64f4bb7e68c1d22d41443dbb3761d60ffd6
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-10-17 10:36:17 +0100

    quiz list whizzer dashboard template

--- students/views.py
+++ students/views.py
@@ -1,26 +1,34 @@
 """
 This file contains the views for the students app.
 """
-from django.utils import timezone
 
-from core.views.base_views import WhizzerBaseTemplateView
-from quiz.models import Quiz, QuizAttempt
+from core.views.base_views import WhizzerBaseListView
+from quiz.models import Quiz
 
 
 # Create your views here.
-class WhizzerDashboardView(WhizzerBaseTemplateView):
+class WhizzerDashboardView(WhizzerBaseListView):
     """
-    This is the view for the creator dashboard.
+    This view lists all the available quizzes that can be taken by a whizzer.
     """
+    model = Quiz
     template_name = 'students/whizzer_dashboard.html'
+    context_object_name = 'quizzes'
+
+    def get_queryset(self):
+        return Quiz.objects.filter(is_active=True)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['quiz_count'] = Quiz.objects.count()
-        context['active_whizzers_count'] = QuizAttempt.objects.filter(
-            start_time__gte=timezone.now() - timezone.timedelta(minutes=30),
-            end_time__isnull=True
-        ).values('whizzer').distinct().count()
         return context
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['quiz_count'] = Quiz.objects.count()
+    #     context['active_whizzers_count'] = QuizAttempt.objects.filter(
+    #         start_time__gte=timezone.now() - timezone.timedelta(minutes=30),
+    #         end_time__isnull=True
+    #     ).values('whizzer').distinct().count()
+    #     return context
 
     pass
