028aef3fc1ff762b95454ca8b8503d11b6ae65ca
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-10-24 12:13:23 +0100

    remodiifed quiz result view

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,14 +3,6 @@ from django.urls import path
 from quiz.views import quiz_views, quiz_creator_views, subject_creator_views
 
 app_name = 'quiz'
-# urlpatterns = [
-#
-#     # path('quiz/<int:pk>/', views.QuizDetailView.as_view(), name='quiz_detail'),
-#
-#     # path('quiz/<int:pk>/start/', views.start_quiz, name='start_quiz'),
-#     # path('quiz/attempt/<int:attempt_id>/', views.take_quiz, name='take_quiz'),
-#     # path('quiz/result/<int:attempt_id>/', views.quiz_result, name='quiz_result'),
-# ]
 
 quiz_urlpatterns = [
     path('', quiz_views.QuizIndexView.as_view(), name='quiz_index'),
@@ -24,8 +16,6 @@ quiz_creator_urlpatterns = [
 ]
 
 subject_creator_urlpatterns = [
-    # path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
-    # path('subjects/<int:pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
     path('subjects/new/', subject_creator_views.SubjectCreateView.as_view(), name='subject_create'),
     path('subjects/update/<uuid:pk>/', subject_creator_views.SubjectUpdateView.as_view(), name='subject_update'),
     path('subjects/delete/<uuid:pk>/', subject_creator_views.SubjectDeleteView.as_view(), name='subject_delete'),
